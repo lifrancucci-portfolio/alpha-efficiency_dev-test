70a6d1012fc5c70e35d578fc9ddeb0f415ecea3e
Lisandro Francucci <lisandro.francucci@gmail.com>
2023-11-12 19:42:51 -0300

    Adjust decorations for Desktop

--- js/main.js
+++ js/main.js
@@ -35,20 +35,24 @@ function contentLoaded() {
     );
     seconds.innerHTML = Math.floor((timeRemaining % (1000 * 60)) / 1000);
   });
+
+  // WE CAN HELP YOU
+  // Replace Image
+  const weCanHelpImg = document.querySelector(
+    "#we-can-help-you .section__image"
+  );
+
+  window.addEventListener("resize", (e) => {
+    if (window.innerWidth > 1200) {
+      weCanHelpImg.src = "img/group-of-people.png";
+    } else {
+      weCanHelpImg.src = "img/group-of-doctors.png";
+    }
+  });
+
   // FAQ
   const faq = document.getElementById("faq");
   const questions = Array.from(faq.querySelectorAll(".faq__item"));
-  // const btns = Array.from(faq.querySelectorAll(".question > img"));
-
-  // btns.forEach((btn) => {
-  //   btn.addEventListener("click", (e) => {
-  //     clearSelection(e.target);
-  //   });
-  // });
-
-  // function clearSelection(selectedElement) {
-  //   console.log(selectedElement);
-  // }
 
   questions.forEach((question) => {
     const btnOpenMenu = question.querySelector(".question > img");
