cd5cf5a28714fa3000962c8bac8164924bb97583
Lisandro Francucci <lisandro.francucci@gmail.com>
2023-11-12 15:40:36 -0300

    Add functionality for countdown timer

--- js/main.js
+++ js/main.js
@@ -6,4 +6,31 @@ function contentLoaded() {
   mobileToggle.addEventListener("click", () => {
     navMenu.classList.toggle("active");
   });
+
+  // COUNTDOWN TIMER
+  // Get elements
+  const countdownTimer = document.querySelector(".hero__countdown-section");
+  const days = countdownTimer.querySelector("#countdown__days");
+  const hours = countdownTimer.querySelector("#countdown__hours");
+  const minutes = countdownTimer.querySelector("#countdown__minutes");
+  const seconds = countdownTimer.querySelector("#countdown__seconds");
+
+  // Countdown End Date
+  let dueDate = new Date("Jan 1, 2024 00:00:00").getTime();
+
+  // Execute every 1 second
+  setInterval(() => {
+    let timeCurrent = new Date().getTime();
+    let timeRemaining = dueDate - timeCurrent;
+
+    // Display results on screen
+    days.innerHTML = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    hours.innerHTML = Math.floor(
+      (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    minutes.innerHTML = Math.floor(
+      (timeRemaining % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    seconds.innerHTML = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+  });
 }
